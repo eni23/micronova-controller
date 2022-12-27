603ec759c89add995a7151e7802a25de014e7ed0
cyrillvw <eni@e23.ch>
2022-12-27 15:45:18 +0100

    backup state since its working now :)

---
+++ README.md
@@ -0,0 +1,71 @@
+# Micronova Controller
+
+## Stove
+
+
+### Protocol
+
+Half-Duplex Serial, 1200 baud, 2 stop bits
+
+#### Reading:
+```
+[LOCATION] [ADDRESS]
+```
+
+Where `LOCATION` is one of:
+
+* `0x00` RAM
+* `0x20` EEPROM
+
+The stove answers with 2 bytes, with the following
+
+```
+[Param+Checksum] [Data]
+
+data = stove_rx_data[1];
+checksum = stove_rx_data[0];
+location = checksum - data;
+```
+#### Writing
+
+```
+[LOCATION] [ADDRESS] [DATA] [CHECKSUM]
+```
+Where `LOCATION` is one of:
+
+* `0x80` RAM
+* `0xA0` EEPROM
+
+The checksum can be calculated as follows:
+
+```
+checksum = LOCATION + ADDRESS + DATA
+
+if ( checksum >= 256 ){
+  checksum = checksum - 256;
+}
+```
+
+### Interface Circuit
+
+#### BOM
+
+* 2x Resistor 150R
+* 1x Resistor 330R
+* 3x Optocoupler PC817 or EL817
+
+#### Circuit
+
+![circuit](doc/schematic-ifboard.png)
+
+#### Connector on stove
+
+![connector](doc/connector.png)
+
+
+### Links that helped me
+
+- https://k3a.me/ir-controller-for-pellet-stove-with-micronova-controller-stufe-e-pellet-aria-ir-telecomando/
+- https://ridiculouslab.altervista.org/en/micronova-protocol-converter/
+- https://stufapellet.forumcommunity.net/?t=57458738
+- https://github.com/philibertc/micronova_controller
--- src/micronova_stove.cpp
+++ src/micronova_stove.cpp
@@ -14,6 +14,14 @@
 #define STOVE_ADDR_FUMES_TEMP     0x3E
 #define STOVE_ADDR_FLAMES_POWER   0x34
 
+// STATE
+#define STOVE_STATE_TURN_ON       0x01
+#define STOVE_STATE_TURN_OFF      0x06
+#define STOVE_STATE_FORCE_OFF     0x00
+
+
+//0 - OFF, 1 - Starting, 2 - Pellet loading, 3 - Ignition, 4 - Work, 5 - Brazier cleaning, 6 - Final cleaning, 7 - Standby, 8 - Pellet missing alarm, 9 - Ignition failure alarm, 10 - Alarms (to be investigated)
+
 
 
 
@@ -30,7 +38,11 @@ class MicronovaStove {
   public:
 
 
+    bool dbg_out = true;
     char stove_rx_data[2];
+    uint8_t last_read_value;
+    uint8_t last_read_param;
+    uint8_t last_read_checksum;
 
 
     MicronovaStove( int rx, int tx, int rx_enable ) {
@@ -42,49 +54,81 @@ class MicronovaStove {
 
     void init(){
       Serial2.begin( 1200, SERIAL_MODE, pin_rx, pin_tx );
+      printf("begin stove serial");
       pinMode( pin_rx_enable, OUTPUT );
       digitalWrite( pin_rx_enable, HIGH ); 
     }
 
 
+
+    /* read functions */
     void enable_rx(){
+      if (dbg_out) printf("enable rx\n");
       digitalWrite( pin_rx_enable, LOW );
     }
 
 
     void disable_rx(){
+      if (dbg_out) printf("disable rx\n");
       digitalWrite( pin_rx_enable, HIGH );
     }
 
 
-    void read_stove_answer(){
-      enable_rx();
-      uint8_t rx_count = 0;
-      stove_rx_data[0] = 0x00;
-      stove_rx_data[1] = 0x00;
-      while ( Serial2.available() ) {
-        stove_rx_data[rx_count] = Serial2.read();
-        rx_count++;
-      }
-      disable_rx();
-    }
+    void read(uint8_t location, uint8_t addr){
 
-    void read_request( uint8_t location, uint8_t addr ){
+      if (dbg_out){
+        printf("read request loc=");
+        printf("0x%02x ", location );
+        printf(", addr=");
+        printf("0x%02x ", addr );
+        printf("\n");
+
+      }
+
       Serial2.write(location);
       delay(1);
       Serial2.write(addr);
+      
+      if (dbg_out) printf("read stove answer\n");
+
+      //return;
+      enable_rx();
+      delay(80);
+      
+      uint8_t rx_count = 0;
+      stove_rx_data[0] = 0x00;
+      stove_rx_data[1] = 0x00;
+      // we have to flush it first to make sure no crap data is on the bus
+      Serial2.flush();
+      while ( Serial2.available() ) {
+        if (rx_count>1){
+          Serial2.flush();
+          break;
+        }
+        stove_rx_data[rx_count] = Serial2.read();
+        rx_count++;
+      }
+      last_read_value = stove_rx_data[1];
+      last_read_checksum = stove_rx_data[0];
+      last_read_param = last_read_checksum - last_read_value;
+      // TODO: check for error with checksum
+      disable_rx();
     }
 
     uint8_t read_ram( uint8_t addr ){
-      read_request( STOVE_OFFSET_RAM_READ, addr );
-      read_stove_answer();
-      uint8_t val = stove_rx_data[1];
-      uint8_t checksum = stove_rx_data[0];
-      uint8_t param = checksum - val;
-      return val
+      read( STOVE_OFFSET_RAM_READ, addr );
+      return last_read_value;
     }
 
-    void write_data( uint8_t location, uint8_t command, uint8_t data ){
+    uint8_t read_eeprom( uint8_t addr ){
+      read( STOVE_OFFSET_EEPROM_READ, addr );
+      return last_read_value;
+    }
+
+
+    /* write functions */
+
+    void write( uint8_t location, uint8_t command, uint8_t data ){
       uint8_t chk = calculate_checksum( location, command, data );
       uint8_t data_to_write[4] = {
         location,
@@ -92,18 +136,22 @@ class MicronovaStove {
         data,
         chk
       };
+      printf("write data=");
       for ( int i = 0; i < 4; i++ ){
-        Serial2.write( data[i] );
+        printf("0x%02x ", data_to_write[i] );
+        Serial2.write( data_to_write[i] );
         delay(1);
       }
+
+      printf("\n");
     }
 
     void write_ram( uint8_t command, uint8_t data ){
-      write_data( STOVE_OFFSET_RAM_WRITE, command, data );
+      write( STOVE_OFFSET_RAM_WRITE, command, data );
     }
 
     void write_eeprom( uint8_t command, uint8_t data ){
-      write_data( STOVE_OFFSET_EEPROM_WRITE, command, data);
+      write( STOVE_OFFSET_EEPROM_WRITE, command, data);
     }
 
     byte calculate_checksum( uint8_t dest, uint8_t addr, uint8_t val ){
