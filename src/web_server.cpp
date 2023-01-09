5ca861f8f74e798a85798a755ce18b7acd4377d1
Cyrill von Wattenwyl <cyrill@peerdom.org>
2023-01-09 21:19:21 +0100

    write api & webfixes

--- src/web_server.cpp
+++ src/web_server.cpp
@@ -13,6 +13,7 @@ uint8_t hexstr2uint(String s){
 }
 
 
+
 void web_init_server(){
 
     // TODO: make port configurable
@@ -60,6 +61,93 @@ void web_init_server(){
         request->send(200, "text/json", answer);
     });
 
+    // read-eeprom, supports multiple addr, comma seperated
+    // TODO: combine with eeprom-code
+    web_server->on("/api/eeprom", HTTP_GET, [](AsyncWebServerRequest *request){
+        String addrs;
+        String answer="{";
+        if (request->hasParam("addr")) {
+            addrs = request->getParam("addr")->value();
+            // check if length is OK
+            if (sizeof(addrs)>80){
+                request->send(500, "text/plain", "err_size");
+                return;
+            }
+            // check for stove lock
+            if( !xSemaphoreTake( xStoveSemaphore, ( TickType_t ) STOVE_SEMAPHORE_WAIT_TICKS ) == pdTRUE ){
+                request->send(500, "text/plain", "err_lock");
+                return;
+            }
+            // split entries, and prepare response json
+            char buff[80];
+            addrs.toCharArray(buff, sizeof(buff));
+            char *ptr;
+            ptr = strtok(buff, ","); 
+            while(ptr != NULL){
+                uint8_t val = hexstr2uint(ptr);
+                stove.read_eeprom(val);
+                answer += "\"" + String(ptr) + "\":\""+stove.last_read_value+"\"";
+                ptr = strtok(NULL, ",");
+                if (ptr) {
+                    answer+=",";
+                }
+            }
+            xSemaphoreGive(xStoveSemaphore);
+        }
+        answer +="}";
+        request->send(200, "text/json", answer);
+    });
+
+    // write-ram
+    // POST only supports one param
+    web_server->on("/api/ram", HTTP_POST, [](AsyncWebServerRequest *request){
+        if (request->hasParam("addr", true) && request->hasParam("value", true)) {
+            String answer = "OK";
+            String addr = request->getParam("addr", true)->value();
+            String value = request->getParam("addr", true)->value();
+            uint8_t addr_s = hexstr2uint(addr);
+            uint8_t value_s = hexstr2uint(value);
+            if( !xSemaphoreTake( xStoveSemaphore, ( TickType_t ) STOVE_SEMAPHORE_WAIT_TICKS ) == pdTRUE ){
+                request->send(500, "text/plain", "err_lock");
+                return;
+            }
+            stove.write_ram(addr_s, value_s);
+            if (request->hasParam("read_response", true)){
+                stove.read_answer();
+                answer = stove.last_read_value;
+            }
+            xSemaphoreGive(xStoveSemaphore);
+            request->send(200, "text/plain", answer);
+        } else {
+            request->send(500, "text/plain", "err_param");
+            return;
+        }
+    });
+    // write-eeprom
+    // TODO: merge with ram-write since mostly dual code
+    web_server->on("/api/eeprom", HTTP_POST, [](AsyncWebServerRequest *request){
+        if (request->hasParam("addr", true) && request->hasParam("value", true)) {
+            String answer = "OK";
+            String addr = request->getParam("addr", true)->value();
+            String value = request->getParam("addr", true)->value();
+            uint8_t addr_s = hexstr2uint(addr);
+            uint8_t value_s = hexstr2uint(value);
+            if( !xSemaphoreTake( xStoveSemaphore, ( TickType_t ) STOVE_SEMAPHORE_WAIT_TICKS ) == pdTRUE ){
+                request->send(500, "text/plain", "err_lock");
+                return;
+            }
+            stove.write_ram(addr_s, value_s);
+            if (request->hasParam("read_response", true)){
+                stove.read_answer();
+                answer = stove.last_read_value;
+            }
+            xSemaphoreGive(xStoveSemaphore);
+            request->send(200, "text/plain", answer);
+        } else {
+            request->send(500, "text/plain", "err_param");
+            return;
+        }
+    });
 
     // turn stove on
     web_server->on("/api/on", HTTP_POST, [](AsyncWebServerRequest *request){
@@ -90,7 +178,7 @@ void web_init_server(){
         }
         // check if state is allowed to turn off, (only in state 1 and 4)
         uint8_t state = stove.get_state();
-        if ( state !=1 || state != 4 ){
+        if ( state != 0x01 && state != 0x04 ){
             xSemaphoreGive(xStoveSemaphore);
             request->send(500, "text/plain", "err_stovestate");
             return;
@@ -110,15 +198,21 @@ void web_init_server(){
                 request->send(500, "text/plain", "err_lock");
                 return;
             }
-            uint8_t cmd;
+            uint8_t cmd=0;
             if (go == "up"){
-                cmd = 0x54;
+                cmd = STOVE_IR_POWERUP;
             }
             if ( go =="down" ) {
-                cmd = 0x50;
+                cmd = STOVE_IR_POWERDOWN;
+            } 
+            if (cmd == 0) {
+                xSemaphoreGive(xStoveSemaphore);
+                request->send(500, "text/plain", "err_param");
+                return;
             }
             for (uint8_t i=0; i<3; i++){
                 stove.write_ram(STOVE_ADDR_IRCOMMAND, cmd);
+                delay(50);
             }
             xSemaphoreGive(xStoveSemaphore);
             request->send(200, "text/json", "OK");
